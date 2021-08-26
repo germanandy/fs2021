f614d269e3932aa4fb9de499dda7f4a03ffe60fc
germanandy <33694548+germanandy@users.noreply.github.com>
2021-08-26 12:49:52 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable resource_group {
   description = "Name of resource group to provision resources"
-  default     = "default"
+  default     = "testdefault"
 }
 
 variable "ibmcloud_region" {
